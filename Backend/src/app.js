6b3c65feb243b1842d4e54cf958ff1b1675a0fad
grxywarxn <lclaros800@gmail.com>
2024-12-07 20:02:57 -0300

    configuracion del cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const sequelize = require('./config/database');
 const productoRoutes = require('./routes/producto.routes');
+const corsMiddleware = require('./middlewares/cors.middleware');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
@@ -8,11 +10,16 @@ require('dotenv').config();
 app.use(express.json());
 
 app.use('/api/productos', productoRoutes);
+app.use(corsMiddleware);
+app.use(cors());
+
 
 sequelize.sync({ force: false })
     .then(() => {
         console.log('Base de datos sincronizada');
     })
     .catch(error => console.error('Error al sincronizar la base de datos:', error));
+
+
 
 module.exports = app;
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,18 @@
 const app = require('./app');
+const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+// Configuración del middleware CORS
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'CORS configurado correctamente' });
+});
+
+app.listen(5000, () => console.log('Servidor en el puerto 5000'));
